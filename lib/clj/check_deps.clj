bbaf67b2d6209835a490e61a3b923ab6dd471185
Maurício Szabo <mauricio.szabo@gmail.com>
2016-06-08 16:09:58 -0300

    Changed to defns

--- lib/clj/check_deps.clj
+++ lib/clj/check_deps.clj
@@ -1,34 +1,37 @@
-{:fq-path-to-relative (fn [fq-path]
-                        (let [root (System/getProperty "user.dir")
-                              size (count root)]
-                          (.substring fq-path size)))
+(ns check-deps)
 
- :symbols-in-project (fn []
-                       (let [rt (clojure.lang.RT/baseLoader)]
-                         (for [namespace (->> (all-ns))
-                               :let [ns-name (.name namespace)]
-                               symbol-map (ns-publics ns-name)
-                               :let [fq-symbol (.val symbol-map)
-                                     file-path (-> fq-symbol meta :file)
-                                     file-str (when file-path
-                                                (some-> rt
-                                                        (.getResource file-path)
-                                                        (.getFile)))]
-                               :when (when file-str (not (re-find #"\.jar!" file-str)))]
-                           (str ns-name "/" (.key symbol-map)))))
+(defn fq-path-to-relative [fq-path]
+  (let [root (System/getProperty "user.dir")
+        size (count root)]
+    (.substring fq-path size)))
 
- :find-dependencies (fn [symbols to-find]
-                      (let [search (fn search [sexp]
-                                     (if (coll? sexp)
-                                       (some search sexp)
-                                       (= sexp to-find)))
-                            have-sym? (fn [s]
-                                        (let [sym (symbol s)
-                                              source (try (clojure.repl/source-fn sym) (catch Exception _))
-                                              quoted (str "`" source)]
-                                          (in-ns (-> s (clojure.string/split #"/") first symbol))
-                                          (search (try (load-string quoted) (catch Exception e)))))]
-                        (filter have-sym? symbols)))}
+(defn symbols-in-project []
+  (let [rt (clojure.lang.RT/baseLoader)]
+    (for [namespace (->> (all-ns))
+          :let [ns-name (.name namespace)]
+          symbol-map (ns-publics ns-name)
+          :let [fq-symbol (.val symbol-map)
+                meta-symbol (meta fq-symbol)
+                file-path (:file meta-symbol)
+                file-str (when file-path
+                           (some-> rt
+                                   (.getResource file-path)
+                                   (.getFile)))]
+          :when (when file-str (not (re-find #"\.jar!" file-str)))]
+      (assoc meta-symbol :fqname (str ns-name "/" (.key symbol-map))))))
+
+(defn find-dependencies [symbols to-find]
+  (let [search (fn search [sexp]
+                 (if (coll? sexp)
+                   (some search sexp)
+                   (= sexp to-find)))
+        have-sym? (fn [s]
+                    (let [sym (-> s :fqname symbol)
+                          source (try (clojure.repl/source-fn sym) (catch Exception _))
+                          quoted (str "`" source)]
+                      (in-ns (-> s (clojure.string/split #"/") first symbol))
+                      (search (try (load-string quoted) (catch Exception e)))))]
+    (filter have-sym? symbols)))
 
 
 ; ((fn [symbols to-find]
