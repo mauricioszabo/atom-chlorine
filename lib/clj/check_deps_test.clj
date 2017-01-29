53ed891ce9fae5513632829d4fad693134656b83
Maurício Szabo <mauricio.szabo@gmail.com>
2017-01-29 11:24:28 -0200

    Added testcase to check for strange edge case

--- lib/clj/check_deps_test.clj
+++ lib/clj/check_deps_test.clj
@@ -6,21 +6,12 @@
   (is (= {:fn "clojure.core/conj"
           :file "clojure/core.clj"
           :line 20}
-         (dissoc (clj-trace "clojure.core$conj__1234" 20) :link))))
+         (dissoc (clj-trace "clojure.core$conj__1234" "core.clj" 20) :link))))
 
 (testing "traces a function with strange name"
   (is (= "clojure.core/conj"
-         (:fn (clj-trace "clojure.core$eval1020$conj__1234" 20))))
+         (:fn (clj-trace "clojure.core$eval1020$conj__1234" "core.clj" 20))))
 
-  (is (= {:fn ""
-          :file "clj/check_deps_test"
-          :line 19}
-        (clj-trace "clj.check_deps_test$eval39348$fn__39349$fn__39350$fn__39351" 19))))
-
-(try
-  ((fn []
-     ((fn [a b]
-        (/ a b))
-      10 0)))
-  (catch Exception e
-    (->> (.getStackTrace e) (into []))))
+  (let [{:keys [fn file]} (clj-trace "clj.check_deps_test$eval39348$fn__39349$fn__39350$fn__39351" "core.clj" 19)]
+    (is (= "clj.check-deps-test/[anon-function]" fn)
+        (re-find #"check_deps_test" file))))
