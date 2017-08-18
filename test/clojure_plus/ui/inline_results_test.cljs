7c43f9147532baeca03950ed64c08f8e0a3e7cdf
Maurício Szabo <mauricio.szabo@gmail.com>
2017-08-18 12:27:32 -0300

    Removed unused code

--- test/clojure_plus/ui/inline_results_test.cljs
+++ test/clojure_plus/ui/inline_results_test.cljs
@@ -19,27 +19,4 @@
                        ["3"]]]
            (inline/parse "[1 2 3]")))))
 
-(deftest inline-result
-  (testing "will show an inline result at specific line"
-    (is (= 1 2))))
-
 (run-tests)
-
-(defn- to-clj-array [element]
-  (if-let [nodes (.-childNodes element)]
-    [(.-nodeName element) (-> js/Array .-prototype .-slice (.call nodes) js->clj)]
-    element))
-
-(comment
- (def r
-   (doto (.createElement js/document "div")
-         (aset "innerHTML"
-               "<div class\"ink tree\"><span class=\"icon icon-chevron-right\"></span><div class=\"header gutted closed\">foo</div><div class=\"body gutted\" style=\"display: none;\"></div></div>")))
- (walk/prewalk to-clj-array r)
- (mapv to-clj-array (to-clj-array r))
- (.log js/console r)
- (-> js/Array .-prototype .-slice (.call (.-childNodes r)) js->clj)
- (concat [] (.-childNodes r))
- (.-children r)
- (.-innerText r))
- ; (.createElement js/document "div"))
