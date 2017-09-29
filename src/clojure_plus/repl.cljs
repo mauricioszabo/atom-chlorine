d82a92bcbe209e32753ab4fc675b213a412bee8d
Maurício Szabo <mauricio.szabo@gmail.com>
2017-09-29 01:04:35 -0300

    Evaluating CLJ and CLJS, with inline

--- src/clojure_plus/repl.cljs
+++ src/clojure_plus/repl.cljs
@@ -1,8 +1,9 @@
 (ns clojure-plus.repl
-  (:require [cljs.reader :as edn]))
+  (:require [cljs.reader :as edn]
+            [clojure-plus.ui.inline-results :as inline]))
 
-(def repl (-> (js/require "../../../clojure-plus")
-              .getCommands .-promisedRepl))
+(defonce repl (-> (js/require "../../../clojure-plus")
+                  .getCommands .-promisedRepl))
 
 (defn execute-cmd
   ([cmd callback] (execute-cmd cmd {} callback))
@@ -13,17 +14,49 @@
                  (callback {:value (edn/read-string val)})
                  (callback {:error (or (.-error %) %)}))))))
 
+(defn- evaluate-cljs [cmd ns-name opts callback]
+  (-> repl
+      (.syncRun cmd ns-name (clj->js opts))
+      (.then #(if-let [val (.-value %)]
+                (callback {:value val})
+                (callback {:error {:cause (or (.-error %) %)}})))))
+
 (defn- evaluate-clj [cmd ns-name opts callback]
-  (let [cmd (str "(try {:value " cmd "}\n(catch Throwable e"
+  (let [cmd (str "(try " cmd "\n(catch Throwable e"
                  "{:error {:cause (str e)"
                  "         :trace (map clj.--check-deps--/prettify-stack (.getStackTrace e))}}))")]
     (-> repl
-        (.syncRun cmd ns-name opts)
+        (.syncRun cmd ns-name (clj->js opts))
         (.then #(if-let [val (.-value %)]
-                  (callback (edn/read-string val))
+                  (callback {:value val})
                   (callback {:error {:cause (or (.-error %) %)}}))))))
 
 (defn evaluate [cmd ns-name opts callback]
   (if (= "cljs" (:session opts))
-    :foo
+    (evaluate-cljs cmd ns-name opts callback)
     (evaluate-clj cmd ns-name opts callback)))
+
+(defn- get-range [editor which]
+  (case which
+    :top-level (-> js/protoRepl .-EditorUtils
+                  (.getCursorInBlockRange editor #js {:topLevel true}))
+    :selection (.getSelectedBufferRange editor)
+    (-> js/protoRepl .-EditorUtils (.getCursorInBlockRange editor))))
+
+(defn- editor-and-range [opts]
+  (let [editor (-> js/atom .-workspace .getActiveTextEditor)]
+    [editor (get-range editor (:scope opts))]))
+
+(defn- create-inline-result [value result]
+  (inline/set-content! result (inline/parse value)))
+
+(defn run-code-on-editor [opts]
+  (println "RUNNING")
+  (let [[editor range] (editor-and-range opts)
+        line (-> range .-end .-row)
+        command (. editor getTextInBufferRange range)
+        result (inline/new-result editor line)]
+    (evaluate command (-> js/protoRepl .-EditorUtils (.findNsDeclaration editor)) opts
+              (fn [{:keys [value error]}]
+                (if value
+                  (create-inline-result value result))))))
--- test/clojure_plus/repl_test.cljs
+++ test/clojure_plus/repl_test.cljs
@@ -2,7 +2,7 @@
   (:require [clojure.test :refer-macros [run-tests testing is deftest async]]
             [clojure-plus.repl :as repl]))
 
-(deftest running-code
+(deftest running-code-in-clj
   (async done
     (repl/execute-cmd '(+ 2 1)
                       "user"
@@ -10,13 +10,34 @@
                         (is (= {:value 3} res))
                         (done)))))
 
-(deftest exceptions
+(deftest exceptions-in-clj
   (async done
     (repl/evaluate "(/ 10 0)"
                    "user"
                    {}
                    (fn [res]
+                     (is (contains? (:error res) :cause))
+                     (is (contains? (:error res) :trace))
+                     (done)))))
+
+(deftest running-code-in-cljs
+  (async done
+    (repl/evaluate "(+ 2 1)"
+                   "user"
+                   {:session "cljs"}
+                   (fn [res]
+                     (is (= {:value 3} res))
+                     (done)))))
+
+(deftest exceptions-in-cljs
+  (async done
+    (repl/evaluate "(throw (js/Error. \"Some Error\"))"
+                   "user"
+                   {:session "cljs"}
+                   (fn [res]
                      (def res res)
                      (is (contains? (:error res) :cause))
-                     (is (contains? (:error res) :stack))
+                    ;  (is (contains? (:error res) :trace))
                      (done)))))
+
+(run-tests)
