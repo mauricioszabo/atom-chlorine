9361e2e6de77a4ec8cff6412e12fe27c4f8684f8
Maurício Szabo <mauricio.szabo@gmail.com>
2018-08-16 17:24:53 -0300

    Updating ClojureScript version, adding check

--- project.clj
+++ project.clj
@@ -1,13 +1,16 @@
 (defproject clojure-plus "0.0.1-SNAPSHOT"
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/clojurescript "1.9.456"]
-                 [expectations "2.1.0"]]
+                 [org.clojure/clojurescript "1.10.339"]
+                 [org.clojure/core.async "0.4.474"]
+                 [repl-tooling "0.0.1-SNAPSHOT"]]
 
   :plugins [[lein-cljsbuild "1.1.5"]]
 
   :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  :jvm-opts ["-Dclojure.server.repl={:port 5556 :accept clojure.core.server/repl}"]
 
   :profiles {:dev {:dependencies [[midje "1.8.3"]
+                                  [check "0.0.1-SNAPSHOT"]
                                   [com.cemerick/piggieback "0.2.1"]
                                   [figwheel-sidecar "0.5.4-6"]]
                    :plugins [[lein-midje "3.2.1"]]}}
