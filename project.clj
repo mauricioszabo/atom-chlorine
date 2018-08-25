9bb365f9bc254959c1a8a2b9566fe718d371ebd0
Maurício Szabo <mauricio.szabo@gmail.com>
2018-08-25 19:01:19 -0300

    Tests with Shadow REPL

--- project.clj
+++ project.clj
@@ -1,5 +1,5 @@
 (defproject clojure-plus "0.0.1-SNAPSHOT"
-  :dependencies [[org.clojure/clojure "1.8.0"]
+  :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.474"]
                  [repl-tooling "0.0.1-SNAPSHOT"]]
@@ -10,9 +10,10 @@
   :jvm-opts ["-Dclojure.server.repl={:port 5556 :accept clojure.core.server/repl}"]
 
   :profiles {:dev {:dependencies [[midje "1.8.3"]
-                                  [check "0.0.1-SNAPSHOT"]
+                                  [check "0.0.2-SNAPSHOT"]
                                   [com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.4-6"]]
+                                  [figwheel-sidecar "0.5.4-6"]
+                                  [thheller/shadow-cljs "2.6.3"]]
                    :plugins [[lein-midje "3.2.1"]]}}
 
   :source-paths ["lib" "src"]
@@ -31,7 +32,7 @@
                         :figwheel true
                         :compiler {:main clojure-plus.core
                                    :output-to "lib/js/main.js"
-                                  ;  :output-dir "lib/js"
+                                   ;  :output-dir "lib/js"
                                    :target :nodejs
                                    :optimizations :simple
                                    :output-wrapper true
