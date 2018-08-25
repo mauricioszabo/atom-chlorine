1719f6e1a06b38563118c869b627dbf635c3a174
Maurício Szabo <mauricio.szabo@gmail.com>
2018-08-25 19:00:35 -0300

    Some REPL rests

--- test/clojure_plus/repl_test.cljs
+++ test/clojure_plus/repl_test.cljs
@@ -1,43 +1,30 @@
 (ns clojure-plus.repl-test
   (:require [clojure.test :refer-macros [run-tests testing is deftest async]]
+            [check.core :refer-macros [check]]
+            [cljs.core.async :as async]
+            [check.async-cljs :refer-macros [def-async-test await!]]
             [clojure-plus.repl :as repl]))
 
-(deftest running-code-in-clj
-  (async done
-    (repl/execute-cmd '(+ 2 1)
-                      "user"
-                      (fn [res]
-                        (is (= {:value 3} res))
-                        (done)))))
+(def-async-test "Running code in Clojure" {}
+  (let [c (async/chan)]
+    (repl/execute-cmd '(+ 2 1) "user" #(async/put! c %))
+    (check (await! c) => {:value 3})))
 
-(deftest exceptions-in-clj
-  (async done
-    (repl/evaluate "(/ 10 0)"
-                   "user"
-                   {}
-                   (fn [res]
-                     (is (contains? (:error res) :cause))
-                     (is (contains? (:error res) :trace))
-                     (done)))))
+(def-async-test "Exceptions in CLJ" {}
+  (let [c (async/chan)]
+    (repl/evaluate "(/ 10 0)" "user" {} #(async/put! c (-> % :value :error)))
+    (check (await! c) =includes=> [:cause :trace])))
 
-(deftest running-code-in-cljs
-  (async done
-    (repl/evaluate "(+ 2 1)"
-                   "user"
+(def-async-test "Running code in CLJS" {}
+  (let [c (async/chan)]
+    (repl/evaluate "(+ 2 1)" "user" {:session "cljs"} #(async/put! c %))
+    (check (await! c) => {:value 3})))
+
+(def-async-test "Exceptions in CLJS" {}
+  (let [c (async/chan)]
+    (repl/evaluate "(throw (js/Error. \"Some Error\"))" "user"
                    {:session "cljs"}
-                   (fn [res]
-                     (is (= {:value 3} res))
-                     (done)))))
-
-(deftest exceptions-in-cljs
-  (async done
-    (repl/evaluate "(throw (js/Error. \"Some Error\"))"
-                   "user"
-                   {:session "cljs"}
-                   (fn [res]
-                     (def res res)
-                     (is (contains? (:error res) :cause))
-                    ;  (is (contains? (:error res) :trace))
-                     (done)))))
+                   #(async/put! c (:error %)))
+    (check (keys (await! c)) => [:cause])))
 
 (run-tests)
