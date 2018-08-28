d6577575e9726be0e0729c095235a12a47abba63
Maurício Szabo <mauricio.szabo@gmail.com>
2018-08-28 19:51:29 -0300

    adding Shadow

--- project.clj
+++ project.clj
@@ -2,39 +2,20 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.474"]
-                 [repl-tooling "0.0.1-SNAPSHOT"]]
+                 [repl-tooling "0.0.1-SNAPSHOT"]
+                 [reagent "0.8.1"]]
 
-  :plugins [[lein-cljsbuild "1.1.5"]]
+  ; :plugins [[lein-cljsbuild "1.1.5"]]
 
-  :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
+  ; :repl-options {:nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
   :jvm-opts ["-Dclojure.server.repl={:port 5556 :accept clojure.core.server/repl}"]
 
   :profiles {:dev {:dependencies [[midje "1.8.3"]
                                   [check "0.0.2-SNAPSHOT"]
-                                  [com.cemerick/piggieback "0.2.1"]
-                                  [figwheel-sidecar "0.5.4-6"]
-                                  [thheller/shadow-cljs "2.6.3"]]
-                   :plugins [[lein-midje "3.2.1"]]}}
+                                  ; [com.cemerick/piggieback "0.2.1"]
+                                  ; [figwheel-sidecar "0.5.4-6"]
+                                  [thheller/shadow-cljs "2.6.3"]]}}
+                   ; :plugins [[lein-midje "3.2.1"]]}}
 
   :source-paths ["lib" "src"]
-  :clean-targets ^{:protect false} ["lib/js"]
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src" "test"]
-                        :figwheel true
-                        :compiler {:main clojure-plus.all-tests
-                                   :output-to "lib/js/main.js"
-                                   :output-dir "lib/js"
-                                   :target :nodejs
-                                   :optimizations :none
-                                   :source-map true}}
-                       {:id "release"
-                        :source-paths ["src"]
-                        :figwheel true
-                        :compiler {:main clojure-plus.core
-                                   :output-to "lib/js/main.js"
-                                   ;  :output-dir "lib/js"
-                                   :target :nodejs
-                                   :optimizations :simple
-                                   :output-wrapper true
-                                   :warnings {:single-segment-namespace false}}}]}
-  :figwheel {})
+  :clean-targets ^{:protect false} ["lib/js"])
