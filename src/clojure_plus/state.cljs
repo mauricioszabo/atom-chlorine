d745a067de08260af3edcf8031cbd4c2bafa3593
Maurício Szabo <mauricio.szabo@gmail.com>
2018-08-30 12:31:18 -0300

    Added "state"

---
+++ src/clojure_plus/state.cljs
@@ -0,0 +1,7 @@
+(ns clojure-plus.state
+  (:require [reagent.core :as r]))
+
+(defonce state (r/atom {:repls {:clj-eval nil
+                                :cljs-eval nil
+                                :clj-aux nil
+                                :cljs-aux nil}}))
