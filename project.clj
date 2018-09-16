1b32e28e346cc78096ead101716445bdad06178e
Maurício Szabo <mauricio.szabo@gmail.com>
2018-09-16 20:22:46 -0300

    Fixed some exports

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :dependencies [[org.clojure/clojure "1.9.0"]
                  [org.clojure/clojurescript "1.10.339"]
                  [org.clojure/core.async "0.4.474"]
-                 [repl-tooling "0.0.1-SNAPSHOT"]
+                 ;[repl-tooling "0.0.1-SNAPSHOT"]
                  [reagent "0.8.1"]]
 
   ; :plugins [[lein-cljsbuild "1.1.5"]]
@@ -12,9 +12,10 @@
 
   :profiles {:dev {:dependencies [[midje "1.8.3"]
                                   [check "0.0.2-SNAPSHOT"]
+                                  [org.clojure/core.async "0.4.474"]
                                   ; [com.cemerick/piggieback "0.2.1"]
                                   ; [figwheel-sidecar "0.5.4-6"]
-                                  [thheller/shadow-cljs "2.6.4"]]}}
+                                  [thheller/shadow-cljs "2.6.6"]]}}
                    ; :plugins [[lein-midje "3.2.1"]]}}
 
   :source-paths ["lib" "src"]
