3a1d1e224841e6c068c30020bd6598bc22f6eba0
Maurício Szabo <mauricio.szabo@gmail.com>
2018-12-15 23:03:58 -0200

    Addiing "goto definition"

---
+++ src/chlorine/features/code.cljs
@@ -0,0 +1,19 @@
+(ns chlorine.features.code
+  (:require [chlorine.ui.atom :as atom]
+            [chlorine.repl :as repl]
+            [chlorine.state :as state]
+            [repl-tooling.features.definition :as definition]))
+
+(defn- open-editor [{:keys [file-name line contents]}]
+  (.. js/atom -workspace (open file-name #js {:initialLine line})))
+
+(defn goto-var []
+  (let [editor (atom/current-editor)
+        var (atom/current-var editor)
+        namespace (repl/ns-for editor)]
+    (.. (some-> @state/state :repls :clj-aux
+                (definition/find-var-definition namespace var))
+        (then (fn [info]
+                (if info
+                  (open-editor info)
+                  (atom/error "Could not find definition for var" "")))))))
