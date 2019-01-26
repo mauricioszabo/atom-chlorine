402703e296c48c6e38de0a8f5da3b1e8cb80c8cd
Maurício Szabo <mauricio.szabo@gmail.com>
2019-01-26 01:25:09 -0200

    Fixed "fixture-app", solves CI problems (probably).

--- integration/fixture-app/project.clj
+++ integration/fixture-app/project.clj
@@ -3,8 +3,6 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojurescript "1.10.339"]
-                 [org.clojure/clojure "1.9.0"]
-                 [thheller/shadow-cljs "2.6.6"]]
+  :dependencies [[thheller/shadow-cljs "2.7.17"]] 
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
--- integration/fixture-app/src/fixture_app/core.cljs
+++ integration/fixture-app/src/fixture_app/core.cljs
@@ -1,4 +1,4 @@
 (ns fixture-app.core)
 
-(defn main [& cli-args])
-
+(defn main [& cli-args]
+  (println "FOOBAR"))
