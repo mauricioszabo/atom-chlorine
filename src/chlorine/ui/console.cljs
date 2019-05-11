29938cfe57eef3972b50d4bd8155ee09f40486d9
Maurício Szabo <mauricio.szabo@gmail.com>
2019-05-11 11:09:32 -0300

    Code to fix Console closing proble

--- src/chlorine/ui/console.cljs
+++ src/chlorine/ui/console.cljs
@@ -1,15 +1,13 @@
 (ns chlorine.ui.console
   (:require [chlorine.ui.inline-results :as inline]
-            [chlorine.aux :as aux]))
-
-(def console-uri "atom://chlorine/console")
+            [chlorine.aux :as aux]
+            [clojure.core.async :as async :include-macros true]))
 
 (defn- from-console-id [^js ink]
   (-> ink .-Console
-      (.fromId "chlorine")
+      (.fromId "chlorine-console")
       (doto
-       (.getTitle (fn [] "Clojure REPL"))
-       (.activate)
+       (.setTitle "Chlorine REPL")
        (.onEval (fn [ed] (prn [:INSERTED ed])))
        (.setModes (clj->js [{:name "chlorine"
                              :default true
@@ -17,11 +15,29 @@
 
 (def console (delay (some-> @inline/ink from-console-id)))
 
-(defn register-console []
-  (.add @aux/subscriptions
-        (.. js/atom -workspace (addOpener (fn [uri]
-                                            (when (= uri console-uri)
-                                              @console))))))
-
 (defn clear []
   (some-> @console .reset))
+
+(declare register-destroy)
+(defn- destroy-fn [^js console callback]
+  (async/go
+   (async/<! (async/timeout 100))
+   (if (-> console .-view .-parentElement .-parentElement)
+     (register-destroy console callback)
+     (callback))))
+
+;; Infer JS problems...
+(defn- on-did-destroy [^js pane callback]
+  (.onDidDestroy pane callback))
+(defn- register-destroy [^js console callback]
+  (async/go-loop []
+    (if-let [pane (.currentPane console)]
+      (on-did-destroy pane #(destroy-fn console callback))
+      (do
+        (async/<! (async/timeout 100))
+        (recur)))))
+
+(defn open-console [split destroy-fn]
+  (.. @console
+      (open #js {:split split :searchAllPanes true})
+      (then #(register-destroy @console destroy-fn))))
