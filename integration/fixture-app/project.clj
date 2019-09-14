e8cfd9ae27daa6c6623e3e1a5d098c95bb42cf7b
CircleCI Automated Build <circleci@chlorine.plugin>
2019-09-14 18:10:07 +0000

    Compiled version for release

--- integration/fixture-app/project.clj
+++
@@ -1,9 +0,0 @@
-(defproject fixture-app "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[thheller/shadow-cljs "2.7.17"]
-                 [org.clojure/clojure "1.10.0"]] 
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
