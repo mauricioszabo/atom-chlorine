3bf72f6b00b521400a044f73c00a337fcd85245c
Maurício Szabo <mauricio.szabo@gmail.com>
2019-09-20 18:40:41 -0300

    Updated tests

--- integration/chlorine-tests/test/chlorine/tests.cljs
+++ integration/chlorine-tests/test/chlorine/tests.cljs
@@ -1,48 +1,23 @@
 (ns chlorine.tests
-  (:require [cljs.core.async :as async :include-macros true :refer [<!]]
-            [chlorine.aux :refer-macros [in-channel]]
-            [clojure.test :refer [testing is deftest async run-all-tests run-tests]]
+  (:require [chlorine.aux :refer-macros [in-channel async async-testing]]
+            [clojure.test :refer [is deftest run-all-tests run-tests] :as t]
             [check.core :refer-macros [check]]
             [promesa.core :as p]
             ["remote" :as remote]))
 
-(defn clj-editor []
-  (in-channel
-    (.. js/atom -workspace
-        (open "/tmp/test.clj" #js {:searchAllPanes true})
-        (then #(async/put! chan %)))))
-
-(defn- editor []
-  (.. js/atom -workspace getActiveTextEditor))
-
-(defn- with-text [^js editor namespace text]
-  (in-channel
-   (.. editor
-       (setText (str "(ns " namespace ")\n\n" text)))
-   (.setSelectedBufferRange editor (clj->js [[2 2] [2 2]]))
-   (async/put! chan :ok)))
-
 (defn- await-for [fun]
-  (in-channel
-   (async/go-loop [n 0]
-     (<! (async/timeout 100))
-     (if (>= n 10)
-       (async/put! chan false)
-       (do
-         (if-let [res (fun)]
-           (async/put! chan res)
-           (do
-             (<! (async/timeout 100))
-             (recur (inc n)))))))))
+  (js/Promise.
+   (fn [resolve]
+     ((fn acc [n resolve]
+        (if (>= n 10)
+          (resolve nil)
+          (if-let [res (fun)]
+            (resolve res)
+            (js/setTimeout #(acc (inc n) resolve) 100))))
+      0 resolve))))
 
 (defn- evaluate-command [command]
   (.. js/atom -commands (dispatch (. js/document -activeElement) command)))
-
-(defn- with-clj [text]
-  (async/go
-   (let [editor (<! (clj-editor))]
-     (evaluate-command "inline-results:clear-all")
-     (with-text editor 'user.test1 text))))
 
 (defn- as-vec [node-list]
   (map #(aget node-list %) (range (.-length node-list))))
@@ -61,68 +36,95 @@
 
 (defn- find-inside-editor [match]
   (find-element "atom-text-editor:not(*[style*='display: none']) div" match))
-
-(defn- connect! []
-  (async/go
-   (evaluate-command "chlorine:connect-clojure-socket-repl")
-   (let [elem (<! (await-for #(. js/document querySelector "input[placeholder=port]")))]
-     ; (aset elem "value" 2233)
-     (.onkeydown elem #js {:key "Enter"}))))
+(defn- find-inside-console [match]
+  (find-element "div.chlorine.console div" match))
 
 (def exist? #(not (nil? %)))
-
-; (defn- timeout [n]
-;   (js/Promise. #(js/setTimeout % n)))
 
 (defn- dispatch-command [command]
   (.. js/atom -commands (dispatch (. js/document -activeElement) command)))
 
-(defn clj-editor1 []
+(defn- connect! []
+  (p/alet [_ (evaluate-command "chlorine:connect-clojure-socket-repl")
+           elem (await-for #(. js/document querySelector "input[placeholder=port]"))]
+     (aset elem "value" 2233)
+     (.onkeydown elem #js {:key "Enter"})))
+
+(defn clj-editor []
   (.. js/atom -workspace (open "/tmp/test.clj" #js {:searchAllPanes true})))
 
+(defn- with-text [^js editor namespace text]
+  (p/alet [_ (.. editor (setText (str "(ns " namespace ")\n\n" text)))]
+    (.setSelectedBufferRange editor (clj->js [[2 2] [2 2]]))))
+
+(defn- with-clj [text]
+  (p/alet [editor (clj-editor)
+           _ (evaluate-command "inline-results:clear-all")]
+    (with-text editor 'user.test1 text)))
+
+(defn- eval-and-check [text command check-fn message]
+  (p/alet [_ (with-clj text)
+           _ (evaluate-command command)
+           contents (check-fn message)]
+    (check (some-> contents .-innerText) => message)))
+
 (deftest connection-and-evaluation
-  (async done
-    (p/alet
-     [_ (evaluate-command "chlorine:disconnect")
-      _ (p/delay 1000)])
+  (async
+    (async-testing "disconnecting editor"
+      (p/alet [_ (evaluate-command "chlorine:disconnect")
+               _ (p/delay 1000)]))
 
-    (testing "connecting to editor"
-      (p/alet
-       [editor (clj-editor1)
-        _ (dispatch-command "chlorine:connect-clojure-socket-repl")]))))
-      ;   message (find-element "div.message" #"REPL connected")])
-      ; (check (<! (find-element "div.message" #"REPL connected")) => exist?))))
-    ;   (<! (async/timeout 1000)))))
+    (async-testing "connecting to editor"
+      (p/alet [editor (clj-editor)
+               _ (connect!)
+               message (find-element "div.message" #"REPL connected")]
+        (check message => exist?)))
 
-#_
-(deftest connection-and-evaluation
-  (async done
-    (async/go
-      (evaluate-command "chlorine:disconnect")
-      (<! (async/timeout 1000))
-      (testing "connecting to editor"
-        (<! (clj-editor))
-        (<! (connect!))
-        (check (<! (find-element "div.message" #"REPL connected")) => exist?)
-        (<! (async/timeout 1000)))
+    (async-testing "evaluates code"
+      (eval-and-check "(str (+ 90 120))" "chlorine:evaluate-top-block"
+                      find-inside-editor #"\"210\""))
 
-      (testing "evaluating simple forms"
-        (with-clj "(str (+ 90 120))")
-        (evaluate-command "chlorine:evaluate-top-block")
-        (check (<! (find-inside-editor #"\"210\"")) => exist?)))
+    (async-testing "go to definition of a var"
+      (p/alet [_ (eval-and-check "defn" "chlorine:go-to-var-definition"
+                                 find-inside-editor #":arglists")]
+        (evaluate-command "core:close")))
 
-      ; (testing "go to definition of a var"
-      ;   (with-clj "defn")
-      ;   (evaluate-command "chlorine:go-to-var-definition")
-      ;   (check (<! (find-inside-editor #":arglists")) => exist?)
-      ;   (evaluate-command "core:close"))
-      ;
-      ; ; (testing "shows definition of var"
-      ; ;   (with-clj "defn")
-      ; ;   (evaluate-command "chlorine:source-for-var")
-      ; ;   (check (<! (find-inside-editor #"fdecl")) => exist?))
+    (async-testing "shows definition of var"
+      (eval-and-check "defn" "chlorine:source-for-var"
+                      find-inside-console #"fdecl"))
 
-    (done)))
+    (async-testing "breaks evaluation"
+      (p/alet [_ (with-clj "(Thread/sleep 2000)")
+               _ (evaluate-command "chlorine:evaluate-top-block")
+               _ (p/delay 400)
+               _ (evaluate-command "chlorine:break-evaluation")
+               contents (find-inside-editor #"Evaluation interrupted")]
+        (check contents => exist?)))
+
+    (async-testing "shows function doc"
+      (eval-and-check "str" "chlorine:doc-for-var"
+                      find-inside-editor
+                      #"With no args, returns the empty string. With one arg x, returns"))))
+
+    ; it('captures exceptions', async () => {})
+    ;   await evalCommand(`(throw (ex-info "Error Number 1", {}))`)
+    ;   assert.ok(await haveSelector(`div.error`))
+    ;   assert.ok(await haveSelector(`span*=Error Number 1`))
+    ;
+    ;   await evalCommand(`(ex-info "Error Number 2", {})`)
+    ;   assert.ok(await haveText(`Error Number 2`))
+    ;
+    ;
+    ; it('allows big strings to be represented', async () => {})
+    ;   await sendCommand('inline-results:clear-all')
+    ;   await evalCommand("(str (range 200))")
+    ;   assert.ok(await haveText("29"))
+    ;   assert.ok(await haveText("..."))
+    ;   // await app.client.click("a*=...")
+    ;   // assert.ok(await haveText("52 53 54"))
+    ;   await sendCommand('inline-results:clear-all')))
+    ;
+
 
 (defn run-my-tests []
   (run-tests))
