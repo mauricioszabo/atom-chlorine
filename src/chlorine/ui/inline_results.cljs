058a733dd3949bae2798789af2f3e54236794b84
Maurício Szabo <mauricio.szabo@gmail.com>
2020-02-18 19:48:42 -0300

    Starting "block decorations" in place of Ink

--- src/chlorine/ui/inline_results.cljs
+++ src/chlorine/ui/inline_results.cljs
@@ -1,5 +1,6 @@
 (ns chlorine.ui.inline-results
   (:require [reagent.core :as r]
+            [chlorine.ui.atom :as atom]
             [repl-tooling.editor-integration.renderer :as render]
             [chlorine.state :refer [state]]))
 
@@ -8,6 +9,7 @@
 (defonce results (atom {}))
 
 (defn get-result [editor row]
+  ; TODO: Remove this, use only div maybe?
   (get-in @results [(.-id editor) row :result]))
 
 (defn all-parsed-results []
@@ -16,28 +18,58 @@
         :when parsed-ratom]
     parsed-ratom))
 
+(defn- discard-old-results! []
+  (doseq [[editor-id v] @results
+          [row {:keys [result div]}] v
+          ; TODO: Feature toggle
+          :when (or (and (not div) (not (some-> result .-view .-view .-isConnected)))
+                    (and div (.isDestroyed result)))]
+    (swap! results update editor-id dissoc row)))
+
+; TODO: Remove Ink
 (defn ^js new-result [^js editor row]
+  (discard-old-results!)
   (when-let [InkResult (some-> ^js @ink .-Result)]
     (let [result (InkResult. editor #js [row row] #js {:type "block"})]
-      (doseq [[editor-id v] @results
-              [row {:keys [result]}] v
-              :when (not (some-> result .-view .-view .-isConnected))]
-        (swap! results update editor-id dissoc row))
       (swap! results assoc-in [(.-id editor) row :result] result)
       result)))
 
+(defn ^js new-inline-result [^js editor [[r1 c1] [r2 c2]]]
+  (discard-old-results!)
+  (let [marker (. editor markBufferRange
+                 (clj->js [[(inc r1) (inc c1)] [(inc r2) (inc c2)]])
+                 #js {:invalidate "inside"})
+        div (doto (. js/document createElement "div")
+                  (aset "classList" "chlorine result-overlay")
+                  (aset "innerHTML" "..."))]
+    (when-let [result (get-result editor r2)]
+      (and (.-isDestroyed result) (.destroy result)))
+
+    (swap! results assoc-in [(.-id editor) r2] {:result marker :div div})
+    (. editor decorateMarker marker #js {:type "block" :position "tail" :item div})))
+
 (defn- create-div! [parsed-ratom]
   (let [div (. js/document createElement "div")]
+    (aset div "classList" "chlorine result-overlay")
     (when (-> parsed-ratom meta :error) (.. div -classList (add "error")))
     (.. div -classList (add "result" "chlorine"))
+    (r/render [:div [render/view-for-result parsed-ratom]] div)
+    div))
+
+(defn- get-or-create-div! [editor row parsed-ratom]
+  (let [div (or (get-in @results [(.-id editor) row :div])
+                (. js/document createElement "div"))]
+    (when (-> parsed-ratom meta :error) (.. div -classList (add "error")))
+    (.. div -classList (add "result"))
     (r/render [:div [render/view-for-result parsed-ratom]] div)
     div))
 
 (defn update-with-result [editor row parsed-ratom]
   (when-let [inline-result (get-result editor row)]
     (swap! results assoc-in [(.-id editor) row :parsed-ratom] parsed-ratom)
-    (let [div (create-div! parsed-ratom)]
-      (.setContent inline-result div #js {:error (-> parsed-ratom meta :error)}))))
+    (let [div (get-or-create-div! editor row parsed-ratom)]
+      (when (.-setContent inline-result)
+        (.setContent inline-result div #js {:error (-> parsed-ratom meta :error)})))))
 
 (defn inline-result [^js editor row parsed-ratom]
   (update-with-result editor row parsed-ratom))
